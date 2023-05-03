cec17551fa0e1e0c94a01c013667018a85832758
marjanhasan <moonmarjanhasan@gmail.com>
2023-05-03 08:20:50 +0600

    loading spinner added with pizza icon spinning

--- src/components/loader/LoadingSpinner.jsx
+++ src/components/loader/LoadingSpinner.jsx
@@ -4,7 +4,9 @@ const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-[calc(100vh-136px)]">
       <p className="text-7xl font-thin">L</p>
-      <div className="w-10 h-10 border-8 border-dashed rounded-full animate-spin mt-5 border-blue-400"></div>
+      <div className="w-10 h-10 rounded-full animate-spin mt-5">
+        <img src="/pizza.png" alt="" />
+      </div>
       <p className="text-7xl font-thin">ading....</p>
     </div>
   );
